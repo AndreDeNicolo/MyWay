29ee6c73b462c9a648a0ec430d402a45eb958b84
AndreDeNick <andrea.denicolo@edu.unife.it>
2021-04-20 12:31:56 +0200

    aggiunta finestra pop up per scegliere percorso

--- app/src/main/java/myway/common/activities/PopUpWindow.java
+++ app/src/main/java/myway/common/activities/PopUpWindow.java
@@ -10,19 +10,27 @@ import android.view.Display;
 import android.view.Gravity;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
+import com.google.android.gms.vision.text.Line;
 import com.myway.myway.R;
 
 public class PopUpWindow extends Activity {
 
     private int screenWidth, screenHeight;
+    private LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.getActionBar().setTitle("Seleziona percorso");
         setContentView(R.layout.activity_pop_up_window);
 
+        setWindowLayout();
         setPopUpWindowProp();
+        addPathButton("dasfjnasfa");
     }
 
     private void setPopUpWindowProp(){
@@ -40,5 +48,18 @@ public class PopUpWindow extends Activity {
         params.y = -20;
 
         getWindow().setAttributes(params);
+    }
+
+    private void setWindowLayout(){
+        linearLayout = new LinearLayout(this);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        LinearLayout.LayoutParams linearLayoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        this.addContentView(linearLayout, linearLayoutParams);
+    }
+
+    public void addPathButton(String pathName){
+        Button b = new Button(this);
+        b.setText(pathName);
+        linearLayout.addView(b);
     }
 }
